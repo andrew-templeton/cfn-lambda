9bf44dd18b4e2370d9c3d5ddc6fd8e30b1e97367
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2016-01-26 15:03:59 +0100

    use style guide

--- deploy.js
+++ deploy.js
@@ -1,24 +1,24 @@
 
-var async = require('async'),
-  format = require('string-format'),
-  path = require('path'),
-  fs = require('fs'),
-  archiver = require('archiver'),
-  stream = require('stream'),
+var format = require('string-format');
+var path = require('path');
+var fs = require('fs');
+var stream = require('stream');
+var async = require('async');
+var archiver = require('archiver');
 
-  DEFAULT_REGION = 'us-east-1',
-  REGIONS =  ['us-east-1', 'us-west-2', 'eu-west-1', 'ap-northeast-1'],
-  RESOURCE_DIR = path.join(__dirname, '..', '..')
-  CFN_LAMBDA_DIR = path.join(__dirname, 'lib'),
-  RESOURCE_INFO = require(path.join(RESOURCE_DIR, 'package.json')),
-  FULL_NAME = format("{}-{}", RESOURCE_INFO.name, RESOURCE_INFO.version.replace(/\./g, '-')),
-  POLICY = fs.readFileSync(path.join(RESOURCE_DIR, 'execution-policy.json')).toString(),
-  TRUST = fs.readFileSync(path.join(CFN_LAMBDA_DIR, 'lambda.trust.json')).toString(),
-  LAMBDA_DESC = format('CloudFormation Custom Resource service for Custom::{name}', RESOURCE_INFO)
-  ACCOUNT_RE = /arn:aws:iam::(.*?):user.*/;
+var DEFAULT_REGION = 'us-east-1';
+var REGIONS =  ['us-east-1', 'us-west-2', 'eu-west-1', 'ap-northeast-1'];
+var RESOURCE_DIR = path.join(__dirname, '..', '..');
+var CFN_LAMBDA_DIR = path.join(__dirname, 'lib');
+var RESOURCE_INFO = require(path.join(RESOURCE_DIR, 'package.json'));
+var FULL_NAME = format("{}-{}", RESOURCE_INFO.name, RESOURCE_INFO.version.replace(/\./g, '-'));
+var POLICY = fs.readFileSync(path.join(RESOURCE_DIR, 'execution-policy.json')).toString();
+var TRUST = fs.readFileSync(path.join(CFN_LAMBDA_DIR, 'lambda.trust.json')).toString();
+var LAMBDA_DESC = format('CloudFormation Custom Resource service for Custom::{name}', RESOURCE_INFO);
+var ACCOUNT_RE = /arn:aws:iam::(.*?):user.*/;
 
 
-var zip_parts = []
+var zip_parts = [];
 
 var archive = archiver('zip');
 
@@ -44,21 +44,26 @@ AWS.config.region = DEFAULT_REGION;
 
 var iam = new AWS.IAM();
 
+var deploy_zip;
+var user_data;
+var role_arn;
+
 function start_deploy() { // Will be emitted when the input stream has ended, ie. no more data will be provided
   console.log('~~~~ Deploying Lambda to all regions (' + REGIONS.join(' ') + '). ~~~~');
-  var base64_zip = Buffer.concat(zip_parts); // Create a buffer from all the received chunks
-  iam.getUser({}, function (err, user_data) {
+  deploy_zip = Buffer.concat(zip_parts); // Create a buffer from all the received chunks
+  iam.getUser({}, function (err, data) {
     if (err) { throw err; }
-    handle_roles(err, user_data, base64_zip);
+    user_data = data;
+    handle_roles(err);
   })
 }
 
-function handle_roles(err, user_data, base64_zip) {
+function handle_roles(err) {
 
-  var ROLE_ARN = format('arn:aws:iam::{}:role/{}',
+  role_arn = format('arn:aws:iam::{}:role/{}',
     user_data.User.Arn.replace(ACCOUNT_RE, '$1'), FULL_NAME);
 
-  console.log(ROLE_ARN);
+  console.log(role_arn);
 
   async.waterfall([
       function(callback) {
@@ -90,7 +95,7 @@ function handle_roles(err, user_data, base64_zip) {
             console.log('Added Policy!');
             console.log("Sleeping 5 seconds for policy to propagate.");
             setTimeout(function() {
-              callback(user_data, base64_zip, ROLE_ARN);
+              callback();
             }, 
             10000);
         });
@@ -98,11 +103,11 @@ function handle_roles(err, user_data, base64_zip) {
     handle_regions); // waterfall 1
   }
 
-function handle_regions(user_data, base64_zip, ROLE_ARN) {
+var handle_regions = function handle_regions() {
   console.log('Beginning deploy of Lambdas to Regions: ' + REGIONS.join(' '));
   async.each(REGIONS,
     function (region, region_callback) {
-      handle_region(region, region_callback, base64_zip, ROLE_ARN);
+      handle_region(region, region_callback);
     },
     function () {
       console.log('~~~~ All done! Lambdas are deployed globally and ready for use by CloudFormation. ~~~~');
@@ -111,7 +116,7 @@ function handle_regions(user_data, base64_zip, ROLE_ARN) {
   });
 }
 
-function handle_region(region, region_callback, base64_zip, ROLE_ARN) { 
+function handle_region(region, region_callback) { 
 
   var RegionAWS = require('aws-sdk');
   RegionAWS.config.region = region;
@@ -123,10 +128,10 @@ function handle_region(region, region_callback, base64_zip, ROLE_ARN) {
   async.waterfall([
       function(callback) {
           lambda.createFunction({
-            Code: {ZipFile: base64_zip},
+            Code: {ZipFile: deploy_zip},
             FunctionName: FULL_NAME,
             Description: LAMBDA_DESC,
-            Role: ROLE_ARN,
+            Role: role_arn,
             Handler: 'index.handler',
             Runtime: 'nodejs',
             Timeout: 300,
@@ -150,7 +155,7 @@ function handle_region(region, region_callback, base64_zip, ROLE_ARN) {
           lambda.updateFunctionConfiguration({
             FunctionName: FULL_NAME,
             Description: LAMBDA_DESC,
-            Role: ROLE_ARN,
+            Role: role_arn,
             Handler: 'index.handler',
             Timeout: 300,
             MemorySize: 128
@@ -171,7 +176,7 @@ function handle_region(region, region_callback, base64_zip, ROLE_ARN) {
           callback();
         }
         else {
-          lambda.updateFunctionCode({FunctionName: FULL_NAME, ZipFile: base64_zip},
+          lambda.updateFunctionCode({FunctionName: FULL_NAME, ZipFile: deploy_zip},
             function(err, data) {
               if (err !== null) {
                 throw err;
